a6f0909f3a797a5e9bbc024c0ad593289e6949e4
Andrew Warfield <andrew.warfield@protonmail.com>
2025-03-23 20:25:19 -0400

    init commit

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use core::str;
+use std::ffi::{CStr, CString};
+use std::os::raw::c_char;
+
+fn main() {
+    //TODO: error handle these
+    let ptr = open_lib("/usr/lib64/libc.so.6").unwrap();
+    let fn_handle = find_symbol(*ptr, "strlen").unwrap();
+
+    call_func(*fn_handle, "hello");
+}
+
+/// Given a path to a .so, will retrieve the openhandle to it.
+/// Essentially a wrapper around dlopen()
+fn open_lib(path: &str) -> Result<Box<*mut libc::c_void>, &str> {
+    unsafe {
+        let c_str = CString::new(path).unwrap();
+        let c_chars: *const c_char = c_str.as_ptr() as *const c_char;
+        let ptr = libc::dlopen(c_chars, libc::RTLD_LAZY);
+        match ptr.is_null() {
+            true => Err(CStr::from_ptr(libc::dlerror()).to_str().unwrap()),
+            false => Ok(Box::new(ptr)),
+        }
+    }
+}
+
+/// Given the handle to the library and the symnbol, returns a pointer to
+/// the function so that it can be called. Essentially a wrapper around
+/// dlsym()
+fn find_symbol(handle: *mut libc::c_void, func: &str) -> Result<Box<*mut libc::c_void>, &str> {
+    unsafe {
+        let c_str = CString::new(func).unwrap();
+        let c_chars: *const c_char = c_str.as_ptr() as *const c_char;
+        let fn_ptr = libc::dlsym(handle, c_chars);
+        match fn_ptr.is_null() {
+            true => Err(CStr::from_ptr(libc::dlerror()).to_str().unwrap()),
+            false => Ok(Box::new(fn_ptr)),
+        }
+    }
+}
+
+fn call_func(fn_handle: *mut libc::c_void, args: &str) {
+    unsafe {
+        // hopefully I don't have to tell you this, but calling an unknown function in some random library
+        // is incredibly dangerous. Be careful about what you call and with what parameters.
+        // This is a good way to get your box exploited or to find undefined behavior.
+        let func: unsafe extern "C" fn(...) -> usize = std::mem::transmute(fn_handle);
+
+        let r = func(args);
+
+        println!("{}", r);
+    }
+}
